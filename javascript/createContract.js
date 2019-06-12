74fe5865874eb0e852c0c29fddb37c3e2757851b
tfeltin <feltin.thomas@gmail.com>
2019-06-12 17:49:28 +0200

    solc + contract creation

---
+++ javascript/createContract.js
@@ -0,0 +1,6 @@
+var compiledContract = solc.compile(fs.readFileSync('./solidity/agreement.sol').toString());
+console.log(compiledContract);
+var abi = compiledContract.contracts[':Agreement'].interface;
+var bytecode = compiledContract.contracts[':Agreement'].bytecode;
+console.log(abi);
+console.log(bytecode);
