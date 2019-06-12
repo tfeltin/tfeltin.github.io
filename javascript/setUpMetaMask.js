e31c958cdf9c621d0f0a76999edcd7c832c64c1b
tfeltin <feltin.thomas@gmail.com>
2019-06-12 17:10:07 +0200

    split js files

---
+++ javascript/setUpMetaMask.js
@@ -0,0 +1,6 @@
+if (typeof web3 !== 'undefined') {
+  web3 = new Web3(web3.currentProvider);
+}else{
+  web3 = new Web3(new Web3.providers.HttpProvider("rinkeby.infura.io/v3/87c66a413df1470abf86a50b4a8bf555"));
+}
+web3.eth.defaultAccount = web3.eth.accounts[0];
