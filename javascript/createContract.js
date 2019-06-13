ea24cb54d501b32b3d2a004fe62b1fedaab4c4a6
tfeltin <feltin.thomas@gmail.com>
2019-06-13 17:38:15 +0200

    browser-solc fix

--- javascript/createContract.js
+++ javascript/createContract.js
@@ -5,6 +5,7 @@ fetch('./solidity/agreement.sol')
   .then(text => source = text);
 
 BrowserSolc.loadVersion("soljson-v0.5.10-nightly.2019.6.13+commit.62bd7032.js", function(compiler) {
+  source = 'contract x { function g() {} }';
   optimize = 1;
   result = compiler.compile(source, optimize);
   console.log(result);
