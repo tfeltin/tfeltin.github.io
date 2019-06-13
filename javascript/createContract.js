fbb82821f3b2345295211b92db4b52d28f4a7843
tfeltin <feltin.thomas@gmail.com>
2019-06-13 17:25:25 +0200

    changed complier version

--- javascript/createContract.js
+++ javascript/createContract.js
@@ -4,7 +4,7 @@ fetch('./solidity/agreement.sol')
   .then(response => response.text())
   .then(text => source = text);
 
-BrowserSolc.loadVersion("soljson-v0.4.6+commit.2dabbdf0.js", function(compiler) {
+BrowserSolc.loadVersion("soljson-v0.5.10-nightly.2019.6.13+commit.62bd7032.js", function(compiler) {
   optimize = 1;
   result = compiler.compile(source, optimize);
   console.log(result);
