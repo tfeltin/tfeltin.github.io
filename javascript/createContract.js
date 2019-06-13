f01d0db99ed2232224875e527df8b336b2eb1145
tfeltin <feltin.thomas@gmail.com>
2019-06-13 18:29:51 +0200

    changing compiler

--- javascript/createContract.js
+++ javascript/createContract.js
@@ -4,8 +4,6 @@ fetch('./solidity/agreement.sol')
   .then(response => response.text())
   .then(text => source = text);
 
-console.log("cc on est là")
-
 BrowserSolc.loadVersion("soljson-v0.5.0+commit.1d4f565a.js", function(compiler) {
     optimize = 1;
     result = compiler.compile(source, optimize);
