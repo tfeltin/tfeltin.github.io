429747e6c6de458262eaabb660cda0640b62a54b
tfeltin <feltin.thomas@gmail.com>
2019-06-13 18:39:26 +0200

    changing compiler version again to 4.5

--- javascript/createContract.js
+++ javascript/createContract.js
@@ -4,7 +4,7 @@ fetch('./solidity/agreement.sol')
   .then(response => response.text())
   .then(text => source = text);
 
-BrowserSolc.loadVersion("soljson-v0.5.0+commit.1d4f565a.js", function(compiler) {
+BrowserSolc.loadVersion("soljson-v0.4.5+commit.b318366e.js", function(compiler) {
     optimize = 1;
     result = compiler.compile(source, optimize);
     console.log(result);
