e23cec485bd1ec93aae9282a4e6d822694c8c354
tfeltin <feltin.thomas@gmail.com>
2019-06-13 14:21:50 +0200

    try

--- javascript/createContract.js
+++ javascript/createContract.js
@@ -1,7 +1,21 @@
-fetch('./solidity/agreement.sol')
-  .then(response => response.text())
-  .then(text => var compiledContract = BrowserSolc.compile(text));
+function readTextFile(file){
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                return allText;
+            }
+        }
+    }
+    rawFile.send(null);
+}
 
+var compiledContract = BrowserSolc.compile(readTextFile('./javascript/agreement.sol').toString());
 console.log(compiledContract);
 var abi = compiledContract.contracts[':Agreement'].interface;
 var bytecode = compiledContract.contracts[':Agreement'].bytecode;
