914a39b893658593e449c5b48e3d8cbcced6aeb6
tfeltin <feltin.thomas@gmail.com>
2019-06-13 13:29:18 +0200

    browser-solc try 2

--- javascript/createContract.js
+++ javascript/createContract.js
@@ -1,9 +1,4 @@
-BrowserSolc.getVersions(function(soljsonSources, soljsonReleases) {
-  console.log(soljsonSources);
-  console.log(soljsonReleases);
-});
-
-var compiledContract = BrowserSolc.compile(fs.readFileSync('./solidity/agreement.sol').toString());
+var compiledContract = BrowserSolc.compile(fs.readFileSync('./javascript/agreement.sol').toString());
 console.log(compiledContract);
 var abi = compiledContract.contracts[':Agreement'].interface;
 var bytecode = compiledContract.contracts[':Agreement'].bytecode;
