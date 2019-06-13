00dd35b11b9cc6ac688c22abc706dcbcb6b25a41
tfeltin <feltin.thomas@gmail.com>
2019-06-13 18:11:06 +0200

    fixing

--- javascript/createContract.js
+++ javascript/createContract.js
@@ -6,7 +6,7 @@ fetch('./solidity/agreement.sol')
 
 console.log("cc on est là")
 
-BrowserSolc.loadVersion("soljson-latest.js", function(compiler) {
+BrowserSolc.loadVersion("soljson-v0.5.9+commit.e560f70d.js", function(compiler) {
   optimize = 1;
   result = compiler.compile(source, optimize);
   console.log(result);
