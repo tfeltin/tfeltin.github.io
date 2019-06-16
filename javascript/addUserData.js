c4f6022f8568c8cc391994ac57b85f9962c326a6
tfeltin <feltin.thomas@gmail.com>
2019-06-16 13:33:33 +0200

    js form try

--- javascript/addUserData.js
+++ javascript/addUserData.js
@@ -103,27 +103,9 @@ const abi = [
 ];
 
 function addUserData(){
-  var ipfsAddress = document.getElementById('ipfs_address')
-
-	const contract = web3.eth.contract(abi);
-	const contractInstance = contract.new({
-		data: '0x' + bytecode['object'],
-    from: web3.eth.defaultAccount,
-    gas: 2000000000
-	}, (err, res) => {
-	    if (err) {
-	        console.log(err);
-	        return;
-	    }
-	    // Log the tx, you can explore status with eth.getTransaction()
-	    console.log(res.transactionHash);
-	    // If we have an address property, the contract was deployed
-	    if (res.address) {
-	        console.log('Contract address: ' + res.address);
-	        // Let's test the deployed contract
-	        testContract(res.address);
-	    }
-	});
+  var ipfsAddress = document.getElementById("ipfs_address").value;
+  console.log(ipfsAddress);
+  await window.web3.estimateGas();
+  cont = window.web3.eth.contract(JSON.parse(abi));
+  contract = cont.at('{{ project.address }}');
 }
-
-document.getElementById('add_user_data').addEventListener("click", function() {addUserData()});
