c20f06cfbba99fca7499e0f2a031c01f8ae1c406
tfeltin <feltin.thomas@gmail.com>
2019-06-16 15:44:24 +0200

    change in address

--- javascript/setUpMetaMask.js
+++ javascript/setUpMetaMask.js
@@ -1,7 +1,10 @@
-if (typeof web3 !== 'undefined') {
-  web3 = new Web3(web3.currentProvider);
-  ethereum.enable();
-}else{
-  web3 = new Web3(new Web3.providers.HttpProvider("rinkeby.infura.io/v3/87c66a413df1470abf86a50b4a8bf555"));
+function async setup(){
+  if (typeof web3 !== 'undefined') {
+    web3 = new Web3(web3.currentProvider);
+    await ethereum.enable();
+    web3.eth.defaultAccount = web3.eth.accounts[0];
+    document.getElementById('address').innerHTML = web3.eth.defaultAccount;
+  } else {
+    web3 = new Web3(new Web3.providers.HttpProvider("rinkeby.infura.io/v3/87c66a413df1470abf86a50b4a8bf555"));
+  }
 }
-web3.eth.defaultAccount = web3.eth.accounts[0];
