d85032a6a1eb3451d90f693ef069d2778522cfbc
tfeltin <feltin.thomas@gmail.com>
2019-06-19 15:51:35 +0200

    change in order

--- javascript/setUp.js
+++ javascript/setUp.js
@@ -102,6 +102,10 @@ const abi = [
 	}
 ];
 
+const cont = window.web3.eth.contract(abi);
+const contract = cont.at("0x745ab2309831426178cea408672fae1160beb996");
+const node = new window.Ipfs();
+
 async function setup(){
   // setup web3 and connect to MetaMask
   if (typeof web3 !== 'undefined') {
@@ -109,33 +113,25 @@ async function setup(){
     await ethereum.enable();
     web3.eth.defaultAccount = web3.eth.accounts[0];
     document.getElementById('address').innerHTML = web3.eth.defaultAccount;
+    contract.getMyData.call((e,r) => {
+      if (!e){
+        while(r.length > 0){
+          var ipfsadd = r.substring(0,46);
+          r = r.substring(46);
+          var a = document.createElement('a');
+          var linkText = document.createTextNode(ipfsadd);
+          a.appendChild(linkText);
+          document.getElementById("myDataloc").appendChild(a);
+          var br = document.createElement('br');
+          document.getElementById("myDataloc").appendChild(br);
+        }
+      } else {
+        console.log(e);
+      }
+    })
   } else {
     web3 = new Web3(new Web3.providers.HttpProvider("rinkeby.infura.io/v3/87c66a413df1470abf86a50b4a8bf555"));
   }
 }
 
 setup();
-const cont = window.web3.eth.contract(abi);
-const contract = cont.at("0x745ab2309831426178cea408672fae1160beb996");
-const node = new window.Ipfs();
-
-function getMyData(){
-  contract.getMyData.call((e,r) => {
-    if (!e){
-      while(r.length > 0){
-        var ipfsadd = r.substring(0,46);
-        r = r.substring(46);
-        var a = document.createElement('a');
-        var linkText = document.createTextNode(ipfsadd);
-        a.appendChild(linkText);
-        document.getElementById("myDataloc").appendChild(a);
-        var br = document.createElement('br');
-        document.getElementById("myDataloc").appendChild(br);
-      }
-    } else {
-      console.log(e);
-    }
-  })
-}
-
-getMyData();
