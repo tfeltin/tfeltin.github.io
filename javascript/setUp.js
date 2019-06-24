1d9c7b6af3dc190c4eee293021394666c367dbb0
tfeltin <feltin.thomas@gmail.com>
2019-06-24 21:34:25 +0200

    new start

--- javascript/setUp.js
+++ javascript/setUp.js
@@ -104,7 +104,7 @@ const abi = [
 
 const $myDataloc = document.querySelector('#myDataloc');
 const cont = window.web3.eth.contract(abi);
-const contract = cont.at("0x7c9d70f0ae85a035308405fcb33e000a9284d072");
+const contract = cont.at("0x9BBDFb64BD1963251EDdb7bCCCaBacF621D4E8e3");
 const node = new window.Ipfs();
 node.once('start', () => {
 	node.id().then((id) => document.getElementById('nodeId').innerHTML = id.id);
