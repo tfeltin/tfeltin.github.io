a1fa8f03a97acee2faa2037615eab0e3ad05e9de
tfeltin <feltin.thomas@gmail.com>
2019-06-26 13:55:19 +0100

    access control added

---
+++ javascript/accessControl.js
@@ -0,0 +1,81 @@
+function grantAccess(){
+	var cid = document.getElementById("grant_cid");
+  var eth_add = document.getElementById("grant_eth");
+
+	web3.eth.getGasPrice((e, gasPrice) => {
+		if (!e){
+			gasPrice = gasPrice.c[0];
+			contract.grantAccess.estimateGas(cid, eth_add, {from: web3.eth.defaultAccount}, (er, gas) => {
+				if (!er){
+					var tx = {
+						from: web3.eth.defaultAccount,
+						gas: gas,
+						gasPrice: gasPrice
+					};
+					contract.grantAccess.sendTransaction(cid, eth_add, tx, (err, result) => {
+						if (!err){
+							var a = document.createElement('a');
+							var success = document.createTextNode("Successfully added file.");
+              a.appendChild(success);
+              a.style.color = 'green';
+							document.getElementById("grant_form").appendChild(a);
+							document.getElementById("grant_form").reset();
+						} else {
+							console.log("Error in transaction");
+							console.log(err);
+						}
+					});
+				} else {
+					console.log("Error while estimating gas");
+					console.log(er);
+				}
+			});
+		} else {
+			console.log("Error while estimating gas price");
+			console.log(e);
+		}
+	});
+}
+
+
+function revokeAccess(){
+	var cid = document.getElementById("revoke_cid");
+  var eth_add = document.getElementById("revoke_eth");
+
+	web3.eth.getGasPrice((e, gasPrice) => {
+		if (!e){
+			gasPrice = gasPrice.c[0];
+			contract.revokeAccess.estimateGas(cid, eth_add, {from: web3.eth.defaultAccount}, (er, gas) => {
+				if (!er){
+					var tx = {
+						from: web3.eth.defaultAccount,
+						gas: gas,
+						gasPrice: gasPrice
+					};
+					contract.revokeAccess.sendTransaction(cid, eth_add, tx, (err, result) => {
+						if (!err){
+							var a = document.createElement('a');
+							var success = document.createTextNode("Successfully added file.");
+              a.appendChild(success);
+              a.style.color = 'green';
+							document.getElementById("revoke_form").appendChild(a);
+							document.getElementById("revoke_form").reset();
+						} else {
+							console.log("Error in transaction");
+							console.log(err);
+						}
+					});
+				} else {
+					console.log("Error while estimating gas");
+					console.log(er);
+				}
+			});
+		} else {
+			console.log("Error while estimating gas price");
+			console.log(e);
+		}
+	});
+}
+
+document.getElementById("grant_button").addEventListener("click", grantAccess);
+document.getElementById("revoke_button").addEventListener("click", revokeAccess);
--- javascript/addData.js
+++ javascript/addData.js
@@ -1,8 +1,6 @@
 function userAddData(){
 	var userFile = document.getElementById("useradd_file");
-	console.log(userFile);
 	var ipfsAddress;
-	console.log(userFile.files[0].name);
   node.add({
           path: userFile.files[0].name,
           content: userFile.files[0]
@@ -10,17 +8,15 @@ function userAddData(){
 				 { wrapWithDirectory: true, progress: (prog) => console.log(`received: ${prog}`) }
 			 )
     .then((response) => {
-      console.log("response=",response);
       ipfsAddress = response[1].hash;
-      console.log(ipfsAddress);
     }).catch((err) => {
       console.error(err)
     });
 	web3.eth.getGasPrice((e, gasPrice) => {
 		if (!e){
 			gasPrice = gasPrice.c[0];
-			contract.userAddData.estimateGas(ipfsAddress, {from: web3.eth.defaultAccount}, (err, gas) => {
-				if (!err){
+			contract.userAddData.estimateGas(ipfsAddress, {from: web3.eth.defaultAccount}, (er, gas) => {
+				if (!er){
 					var tx = {
 						from: web3.eth.defaultAccount,
 						gas: gas,
@@ -28,9 +24,10 @@ function userAddData(){
 					};
 					contract.userAddData.sendTransaction(ipfsAddress, tx, (err, result) => {
 						if (!err){
-							console.log(result);
 							var a = document.createElement('a');
-							var linkText = document.createTextNode("Successfully added file.".green);
+							var linkText = document.createTextNode("Successfully added file.");
+							a.appendChild(linkText);
+							a.style.color = 'green';
 							document.getElementById("useradd_form").appendChild(a);
 							document.getElementById("useradd_form").reset();
 						} else {
@@ -40,7 +37,7 @@ function userAddData(){
 					});
 				} else {
 					console.log("Error while estimating gas");
-					console.log(err);
+					console.log(er);
 				}
 			});
 		} else {
@@ -54,24 +51,21 @@ function userAddData(){
 function spAddData(){
 	var userAddress = document.getElementById("user_eth_address").value;
 	var spFile = document.getElementById("spadd_file");
-	console.log(spFile);
 	var ipfsAddress;
   node.add({
           path: spFile.files[0].name,
           content: spFile.files[0]
         }, { wrapWithDirectory: true, progress: (prog) => console.log(`received: ${prog}`) })
     .then((response) => {
-      console.log(response);
       ipfsAddress = response[1].hash;
-      console.log(ipfsAddress);
     }).catch((err) => {
       console.error(err)
     });
 	web3.eth.getGasPrice((e, gasPrice) => {
 		if (!e){
 			gasPrice = gasPrice.c[0];
-			contract.spAddData.estimateGas(ipfsAddress, userAddress, {from: web3.eth.defaultAccount}, (err, gas) => {
-				if (!err){
+			contract.spAddData.estimateGas(ipfsAddress, userAddress, {from: web3.eth.defaultAccount}, (er, gas) => {
+				if (!er){
 					const tx = {
 						from: web3.eth.defaultAccount,
 						gas: gas,
@@ -79,9 +73,10 @@ function spAddData(){
 					};
 					contract.spAddData.sendTransaction(ipfsAddress, userAddress, tx, (err, result) => {
 						if (!err){
-							console.log(result);
 							var a = document.createElement('a');
-							var linkText = document.createTextNode("Successfully added file.".green);
+							var linkText = document.createTextNode("Successfully added file.");
+							a.appendChild(linkText);
+							a.style.color = 'green';
 							document.getElementById("spadd_form").appendChild(a);
 							document.getElementById("spadd_form").reset();
 						} else {
@@ -91,7 +86,7 @@ function spAddData(){
 					});
 				} else {
 					console.log("Error while estimating gas");
-					console.log(err);
+					console.log(er);
 				}
 			});
 		} else {
